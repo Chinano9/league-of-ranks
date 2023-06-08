86536e5e9d98624fa50a07901d0e7b699fa7721b
Fernando Ponce Solis <ponceconc@outlook.com>
2023-06-07 20:47:55 -0600

    configured project

--- ranks/models.py
+++ ranks/models.py
@@ -1,32 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
-import requests
+
 
 # Create your models here.
 
 class Ranking(models.Model):
     name = models.CharField(max_length=30)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Summoner(models.Model):
     summonerId = models.CharField(primary_key=True, max_length=50)
     name = models.CharField(max_length=30)
     win_rate = models.FloatField()
     rank = [ 
-            'iron',
-            'bronze',
-            'silver',
-            'gold',
-            'platinum',
-            'diamond',
-            'master',
-            'grandmaster',
-            'challenger'
+            ('ir','iron'),
+            ('br','bronze'),
+            ('sl','silver'),
+            ('gl','gold'),
+            ('pl','platinum'),
+            ('di','diamond'),
+            ('ms','master'),
+            ('gm','grandmaster'),
+            ('ch','challenger')
         ]
     ranking = models.CharField(max_length=15, choices=rank)
     division = models.CharField(max_length=1)
     league_points = models.IntegerField()
 
 class SummonerRanking(models.Model):
-    ranking = models.ForeignKey(Ranking)
-    summonerId = models.ForeignKey(Summoner) 
+    ranking = models.ForeignKey(Ranking, on_delete=models.CASCADE)
+    summonerId = models.ForeignKey(Summoner, on_delete=models.CASCADE) 
