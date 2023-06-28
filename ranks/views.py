611b8a1d87a5c513b739326f87068bcff8c497d5
Fernando Ponce Solis <ponceconc@outlook.com>
2023-06-27 22:50:07 -0600

    added env variables

--- ranks/views.py
+++ ranks/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
+from django.shortcuts import get_object_or_404
 from .models import Summoner, SummonerRanking, Ranking
 from .serializers import SummonerSerializer, SummonerRankingSerializer, RankingSerializer
+import requests
+import os
 
+
+"""http://ddragon.leagueoflegends.com/cdn/13.12.1/img/profileicon/685.png"""
 # Create your views here.
 
 class SummonerByNameView(APIView):
@@ -20,7 +25,10 @@ class SummonerByNameView(APIView):
             if summoner.name.lower().find(request.name.lower()) >= 0:
                 summoners.append(summoner)
         serializer = SummonerSerializer(summoners)
-        #if not serializer.data:
+        if not serializer.data:
+            url = f'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{request.name.lower()}'
+            headers = {'X-Riot-Token': os.environ.get('RIOT_API_KEY')}
+            serializer.data = requests.get(url, headers)
         #    pass
         return Response(serializer.data)
 
@@ -33,6 +41,8 @@ class SummonerByIdView(APIView):
         """
         Lists all the summoner information
         """
+        summoner = Summoner.get_object_or_404(request.id)
+        return Response(summoner)
     def post(self, request, format=None):
         """
         Records this summoner to the database
